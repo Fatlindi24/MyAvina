47150481b6f2adbd1796b3a0f7d06db9d27a5fd4
Rinor-Up-Bizz <info@upbizz.com>
2025-07-29 15:29:51 +0200

    delete

--- backend/src/middleware/authMiddleware.js
+++
@@ -1,7 +0,0 @@
-// src/middleware/authMiddleware.js
-exports.requireLogin = (req, res, next) => {
-  if (!req.session.userId) {
-    return res.status(401).json({ message: 'Unauthorized. Please login.' });
-  }
-  next();
-};
